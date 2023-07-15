0bad85b5886d69fbdc1fa8f5681465e41bb598cf
Ostap <revakooa@gmail.com>
2023-07-16 01:16:42 +0300

    add adapter to show artists.

---
+++ app/src/main/java/com/ostapr/lastfm/main/ArtistsListAdapter.kt
@@ -0,0 +1,54 @@
+package com.ostapr.lastfm.com.ostapr.lastfm.main
+
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.request.CachePolicy
+import com.ostapr.lastfm.R
+import com.ostapr.model.Artist
+
+class ArtistsListAdapter : PagingDataAdapter<Artist, ArtistPosterViewHolder>(MovieDiffCallBack()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArtistPosterViewHolder =
+        ArtistPosterViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.artist_item, parent, false)
+        )
+
+    override fun onBindViewHolder(holder: ArtistPosterViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+class MovieDiffCallBack : DiffUtil.ItemCallback<Artist>() {
+    override fun areItemsTheSame(oldItem: Artist, newItem: Artist): Boolean {
+        return oldItem.mbid == newItem.mbid
+    }
+
+    override fun areContentsTheSame(oldItem: Artist, newItem: Artist): Boolean {
+        return oldItem == newItem
+    }
+}
+
+class ArtistPosterViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private val posterImageView = view.findViewById<ImageView>(R.id.imagePoster)
+    private val nameTextView = view.findViewById<TextView>(R.id.textName)
+    private val rankTextView = view.findViewById<TextView>(R.id.textRank)
+
+
+    fun bind(artist: Artist?) {
+        artist?.pickImageUrl()?.let { url ->
+            posterImageView.load(url) {
+                memoryCachePolicy(CachePolicy.ENABLED)
+            }
+        }
+        nameTextView.text = artist?.name ?: ""
+        rankTextView.text = "${artist?.rank}" ?: ""
+    }
+}
--- core/network/src/main/java/com/ostapr/core/network/TopArtistsPagingSource.kt
+++ core/network/src/main/java/com/ostapr/core/network/TopArtistsPagingSource.kt
@@ -3,21 +3,43 @@ package com.ostapr.core.network
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.ostapr.model.Artist
+import retrofit2.HttpException
+import java.io.IOException
 
 
 internal class TopArtistsPagingSource(
     private val artistsService: ArtistsService
-): PagingSource<Int, Artist>() {
-    override val jumpingSupported: Boolean
-        get() = super.jumpingSupported
-    override val keyReuseSupported: Boolean
-        get() = super.keyReuseSupported
-
-    override fun getRefreshKey(state: PagingState<Int, Artist>): Int? {
-        TODO("Not yet implemented")
-    }
+) : PagingSource<Int, Artist>() {
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Artist> {
+        val page = params.key ?: 1
+        return try {
+            val response =
+                artistsService.getGeoTopArtists(
+                    UKRAINE_COUNTRY_CODE,
+                    page = page,
+                    itemsPerPage = params.loadSize
+                )
 
+            LoadResult.Page(
+                data = response.topArtists.artists.map { it.convertToDomainClass() },
+                nextKey = page + 1,
+                prevKey = if (page == 1) null else page - 1
+            )
+        } catch (e: IOException) {
+            LoadResult.Error(e)
+        } catch (e: HttpException) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Artist>): Int? {
+        val anchorPos = state.anchorPosition ?: return 1 // load for the first time
+        return state.closestPageToPosition(anchorPos)?.prevKey?.plus(1)
+            ?: state.closestPageToPosition(anchorPos)?.nextKey?.minus(1)
+    }
+
+    companion object {
+        const val UKRAINE_COUNTRY_CODE = "uk"
     }
 }
